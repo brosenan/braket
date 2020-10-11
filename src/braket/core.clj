063ab8efe00d259592fef577ba0f817dc79dafbc
Boaz Rosenan <brosenan@brosenan-macbookpro.roam.corp.google.com>
2020-10-11 09:08:13 +0300

    unification-improvements

--- src/braket/core.clj
+++ src/braket/core.clj
@@ -2,11 +2,14 @@
   (:require [clojure.walk :as walk]))
 
 (defn binding? [a]
-  (and (list? a)
+  (and (seq? a)
        (int? (first a))))
 
+(defn split-binding [[var & options]]
+  [var options])
+
 (defn quoted-symbol? [a]
-  (and (list? a)
+  (and (seq? a)
        (= (first a) 'quote)
        (symbol? (second a))))
 
@@ -26,26 +29,69 @@
                                false))
          (int? type) (subtype? sub (conj (bindings type) type) bindings assumptions)
          (quoted-symbol? type) false
-         (and (list? sub)
-              (list? type)
+         (and (seq? sub)
+              (seq? type)
               (= (count sub)
                  (count type))) (every? identity
                                         (map #(subtype? %1 %2 bindings assumptions) sub type))
          :else false)))
 
-(defn unify [fresh a b]
+(defn extract-bindings [expr]
   (cond
-    (= a b) a
-    (and (binding? a)
-         (binding? b)) (concat a (walk/prewalk-replace {(first b) (first a)} (rest b)))
-    (binding? a) (concat a (list b))
-    (binding? b) (concat b (list a))
-    (quoted-symbol? a) (list (fresh) a b)
-    (and (list? a)
-         (list? b)
-         (= (count a) (count b))) (map (partial unify fresh) a b)
-    :else (list (fresh) a b)))
+    (quoted-symbol? expr) [expr {}]
+    (binding? expr) (let [[var options] (split-binding expr)
+                          [options' bindings] (extract-bindings options)]
+                      [var (-> bindings (merge {var options'}))])
+    (seq? expr) (let [pairs (map extract-bindings expr)]
+                  [(map first pairs)
+                   (->> pairs (map second) (apply merge-with concat))])
+    :else [expr {}]))
+
+(defn embed-bindings [expr bindings]
+  (cond
+    (and
+     (int? expr)
+     (contains? bindings expr)) (conj (embed-bindings (bindings expr)
+                                                      (-> bindings (dissoc expr)))
+                                      expr)
+    (seq? expr) (map #(embed-bindings % bindings) expr)
+    :else expr))
+
+(defn dedup-bindings* [expr]
+  (cond
+    (empty? expr) (list)
+    :else (let [dedupped (dedup-bindings* (rest expr))]
+            (cond
+              (some identity (for [d dedupped]
+                               (subtype? (first expr) d))) dedupped
+              :else (conj dedupped (first expr))))))
+
+(defn dedup-bindings [bindings]
+  (->> bindings
+       (map (fn [[var expr]]
+              [var (->> expr
+                        (filter #(not= % var))
+                        (map #(embed-bindings % bindings))
+                        reverse
+                        dedup-bindings*
+                        reverse
+                        dedup-bindings*
+                        extract-bindings first)]))
+       (into {})))
 
 (defn nat-gen [init]
   (let [c (atom init)]
     (partial swap! c inc)))
+
+(defn unify [gen a b]
+  (let [a (cond (binding? a) a
+                :else (list (gen) a))
+        b (cond (binding? b) b
+                :else (list (gen) b))
+        [a a-bindings] (extract-bindings a)
+        [b b-bindings] (extract-bindings b)
+        b-bindings (walk/postwalk-replace {b a} b-bindings)
+        b-bindings (walk/postwalk-replace {b a} b-bindings)
+        bindings (merge-with concat a-bindings b-bindings)
+        bindings (dedup-bindings bindings)]
+    (embed-bindings a bindings)))
--- test/braket/core_test.clj
+++ test/braket/core_test.clj
@@ -24,9 +24,49 @@
  (subtype? '('a (2 'b ('a 2))) '(1 ('a 1) 'b 'c)) => true)
 
 (fact
- (unify (nat-gen 0) ''a ''a) => ''a
+ (extract-bindings ''a) => '['a {}]
+ (extract-bindings '('a 'b)) => '[('a 'b) {}]
+ (extract-bindings '(1 'a)) => '[1 {1 ('a)}]
+ (extract-bindings '((1 'a) (2 'b))) => '[(1 2) {1 ('a)
+                                                 2 ('b)}]
+ (extract-bindings '((1 'a) (1 'b))) => '[(1 1) {1 ('a 'b)}]
+ (extract-bindings '(1 'a (2 'b))) => '[1 {1 ('a 2)
+                                           2 ('b)}])
+
+(fact
+ (embed-bindings ''a {}) => ''a
+ (embed-bindings 1 '{1 ('a)}) => '(1 'a)
+ (embed-bindings '(1 2) '{1 ('a)
+                          2 ('b)}) => '((1 'a) (2 'b))
+ (embed-bindings 1 '{1 ('a 2)
+                     2 ('b)}) => '(1 'a (2 'b))
+ (embed-bindings 1 '{1 ('a ('b 1))}) => '(1 'a ('b 1)))
+
+(fact
+ (dedup-bindings {1 '('a 'b)}) => {1 '('a 'b)}
+ (dedup-bindings {1 '('a 'a)}) => {1 '('a)}
+ (dedup-bindings {1 '('a)
+                  2 '('a 1)}) => {1 '('a)
+                                  2 '('a)}
+ (dedup-bindings {1 '('a)
+                  2 '(1 'a)}) => {1 '('a)
+                                  2 '(1)}
+ (dedup-bindings {1 '('a)
+                  2 '('a 'b)
+                  3 '(1 2)}) => {1 '('a)
+                                 2 '('a 'b)
+                                 3 '(2)}
+ (dedup-bindings {1 '('a ('b 1) 2)
+                  2 '('a ('b 2) 'c)}) => {1 '(2)
+                                          2 '('a ('b 2) 'c)}
+ (dedup-bindings {1 '(2 'a ('b 1))
+                  2 '('a ('b 2) 'c)}) => {1 '(2)
+                                          2 '('a ('b 2) 'c)}
+ (dedup-bindings {1 '('a ('b 1) 1)}) => {1 '('a ('b 1))})
+
+(fact
+ (unify (nat-gen 0) ''a ''a) => '(1 'a)
  (unify (nat-gen 0) ''a ''b) => '(1 'a 'b)
- (unify (nat-gen 0) '('a 'b) '('c 'b)) => '((1 'a 'c) 'b)
  (unify (nat-gen 3) '(1 'a ('b 1)) ''c) => '(1 'a ('b 1) 'c)
- (unify (nat-gen 3) ''a '(1 'b ('c 1))) => '(1 'b ('c 1) 'a)
+ (unify (nat-gen 3) ''a '(1 'b ('c 1))) => '(4  'a 'b ('c 4))
  (unify (nat-gen 3) '(1 'a ('b 1)) '(2 'c ('d 2))) => '(1 'a ('b 1) 'c ('d 1)))
